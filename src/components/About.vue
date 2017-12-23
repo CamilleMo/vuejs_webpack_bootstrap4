63cd79867e57c94682661d223102db227606368f
Camille Moatti <camille.moatti@gmail.com>
2017-12-23 21:02:42 +0100

    adddded a footer and one correction

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <div class="container">
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+        <br>
+      <div class="row">
+              <div class="col-md-12">
+              <h1 class="text-center">{{ msg }}</h1>
+              <br>
+              <div class="card">
+                  <div class="card-header">
+                    This page will show how to fetch an API
+                  </div>
+                  <div class="card-body">
+                    <h4 class="card-title">Download a TODO list</h4>
+                    <p class="card-text">If you don't want to, you can always return Home !</p>
+                    <router-link class="btn btn-primary" v-bind:to="'/'">Home</router-link>
+                  </div>
+                </div>
+     
+              </div>
+            </div>
+          </div>
+
 </template>
 
 <script>
@@ -34,17 +37,6 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+  font-weight: bold;
 }
 </style>
