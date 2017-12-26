002fe87cbf1f8f128e797b12098c36ce266e52a3
Camille Moatti <camille.moatti@gmail.com>
2017-12-26 16:48:10 +0100

    added a working fetch method

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,9 +14,37 @@
                     <h4 class="card-title">Download a TODO list</h4>
                     <p class="card-text">If you don't want to, you can always return Home !</p>
                     <router-link class="btn btn-primary" v-bind:to="'/'">Home</router-link>
+                    <br>
+                    <br>
+                    <button class="btn btn-primary" v-on:click="fetch_api">Fetch API</button>
                   </div>
                 </div>
-     
+                   <div v-if="remote">
+                    <br>
+                    The result :
+                    <table class="table table-sm table-hover">
+                      <thead>
+                        <tr>
+                          <th>UserID</th>
+                          <th>ID</th>
+                          <th>Title</th>
+                          <th>Completed</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        <tr v-for="todo in todos">
+                          <!-- <th scope="row">1</th> -->
+                          <td>{{ todo.userId }}</td>
+                          <td>{{ todo.id }}</td>
+                          <td>{{ todo.title }}</td>
+                          <td>{{ todo.completed }}</td>
+                        </tr>
+
+                      </tbody>
+                      </table>
+
+                  </div>
               </div>
             </div>
           </div>
@@ -24,12 +52,37 @@
 </template>
 
 <script>
+var Vue = require('vue');
 export default {
   name: 'About',
   data () {
     return {
-      msg: 'This is the about page !'
+      msg: 'This is the about page !',
+      remote: false,
+      todos: {
+            }
     }
+  },
+  methods: {
+  fetch_api: function(){
+    var _this = this;
+
+    fetch("http://jsonplaceholder.typicode.com/todos").then(function (response) {
+            return response.json();
+              }).then((result) => {
+                  
+                  this.todos_raw =  result;
+                  //alert(result);
+                  // this.todos = [];
+                  var i;
+                  for (i = 0; i < this.todos_raw.length; i++) {
+                    _this.$set(this.todos, i, this.todos_raw[i]);
+                  };
+              });
+              
+              this.remote = true;
+  }
+
   }
 }
 </script>
